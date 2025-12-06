021e4b62856dde7f64fe86b8a34ef453b6d39d25
Lsy9109704 <383202795@qq.com>
2025-12-06 18:51:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -93,8 +93,8 @@ clf, models_info, scaler, df = build_pipeline()
 with st.sidebar:
     st.header("🎛️ Sensor Input")
     st.info("Input raw values ($F/F_0 - 1$)")
-    mpa_val = st.number_input("Sensor 1 (MPA)", value=-0.00250, format="%.5f", step=0.0001)
-    flu_val = st.number_input("Sensor 2 (Flu)", value=0.00000, format="%.5f", step=0.0001)
+    mpa_val = st.number_input("Sensor 1 (1@Pt@MPA)", value=-0.00250, format="%.5f", step=0.0001)
+    flu_val = st.number_input("Sensor 2 (aggregate of 1 on Pt@MPA)", value=0.00000, format="%.5f", step=0.0001)
     st.markdown("---")
     analyze_btn = st.button("🚀 Analyze", type="primary")
 
@@ -194,3 +194,4 @@ if analyze_btn:
             ax.grid(True, linestyle=':', alpha=0.5)
             
             st.pyplot(fig)
+
